7a45eb4fb04695ef6402bccd1a48c7bce7a57bf1
azusaanson <yth041512@gmail.com>
2023-07-22 14:30:14 +0900

    remapping

--- src/token/MyToken.sol
+++ src/token/MyToken.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.19;
 
 import { IMyToken } from "./IMyToken.sol";
-import { IERC20 } from "../../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
-import { ERC20 } from "../../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
-import { ERC20Votes, ERC20Permit } from "../../lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { ERC20Votes, ERC20Permit } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
 contract MyToken is IMyToken, ERC20Votes {
     // ============ Constants ============
