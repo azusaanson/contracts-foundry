7cd2ad1acb3b5375e7e469b995a72b0d5f3b0b7d
azusaanson <yth041512@gmail.com>
2023-07-22 16:27:07 +0900

    add positive unit test

---
+++ test/token/MyTokenERC20.t.sol
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "forge-std/Test.sol";
+import "../../src/token/MyToken.sol";
+import "../utils/Helper.sol";
+
+contract MyTokenERC20Test is Test, Helper {
+    MyToken public myToken;
+    address public distributor = makeAddr("distributor");
+    uint256 public constant INITIAL_SUPPLY = 1_000_000_000;
+
+    function setUp() public {
+        myToken = new MyToken(distributor);
+        assertEq(myToken.totalSupply(), INITIAL_SUPPLY);
+        assertEq(myToken.balanceOf(distributor), INITIAL_SUPPLY);
+    }
+
+    function testBasicView() public {
+        assertEq(myToken.name(), 'MyToken');
+        assertEq(myToken.symbol(), 'MYTOKEN');
+        assertEq(myToken.decimals(), 18);
+    }
+
+    function testTransfer(uint256 amount, uint256 amount2) public {
+        address to = makeAddr("to");
+        address to2 = makeAddr("to2");
+
+        vm.startPrank(distributor);
+        amount = zeroMod(amount, INITIAL_SUPPLY);
+        myToken.transfer(to, amount);
+        assertEq(myToken.balanceOf(to), amount);
+        assertEq(myToken.balanceOf(distributor), INITIAL_SUPPLY - amount);
+        vm.stopPrank();
+
+        vm.startPrank(to);
+        amount2 = zeroMod(amount2, amount);
+        myToken.transfer(to2, amount2);
+        assertEq(myToken.balanceOf(to2), amount2);
+        assertEq(myToken.balanceOf(to), amount - amount2);
+        vm.stopPrank();
+    }
+
+    function testAllowance(uint256 amount, uint256 allowAmount, uint256 increaseAmount, uint256 decreaseAmount, uint256 toAmount) public {
+        address owner = makeAddr("owner");
+        address spender = makeAddr("spender");
+        address to = makeAddr("to");
+
+        vm.startPrank(distributor);
+        amount = zeroMod(amount, INITIAL_SUPPLY);
+        myToken.transfer(owner, amount);
+        vm.stopPrank();
+
+        vm.startPrank(owner);
+        allowAmount = zeroMod(allowAmount, amount);
+        myToken.approve(spender, allowAmount);
+        assertEq(myToken.allowance(owner, spender), allowAmount);
+
+        increaseAmount = zeroMod(increaseAmount, amount - allowAmount);
+        myToken.increaseAllowance(spender, increaseAmount);
+        allowAmount = allowAmount + increaseAmount;
+        assertEq(myToken.allowance(owner, spender), allowAmount);
+
+        decreaseAmount = zeroMod(decreaseAmount, allowAmount);
+        myToken.decreaseAllowance(spender, decreaseAmount);
+        allowAmount = allowAmount - decreaseAmount;
+        assertEq(myToken.allowance(owner, spender), allowAmount);
+        vm.stopPrank();
+
+        vm.startPrank(spender);
+        toAmount = zeroMod(toAmount, allowAmount);
+        myToken.transferFrom(owner, to, toAmount);
+        assertEq(myToken.balanceOf(to), toAmount);
+        assertEq(myToken.balanceOf(owner), amount - toAmount);
+        vm.stopPrank();
+    }
+}
---
+++ test/utils/Helper.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract Helper {
+    function zeroMod(uint256 num1, uint256 num2) internal pure returns (uint256) {
+        if (num1 == 0 || num2 == 0) {
+            return 0;
+        }
+        return num1 % num2;
+    }
+}
