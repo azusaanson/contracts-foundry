ef455493e403fcbcbefd86db052fbd008ce0e4e3
azusaanson <yth041512@gmail.com>
2023-07-27 19:40:15 +0900

    fix error by linter bug

--- test/token/MyToken.t.sol
+++ test/token/MyToken.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "forge-std/Test.sol";
+import "../../lib/forge-std/src/Test.sol";
 import "../../src/token/MyToken.sol";
 import "../utils/Helper.sol";
 
