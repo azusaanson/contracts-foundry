93c8ac7bee7728beb2c7a300e6d7a99841f6b6c3
azusaanson <yth041512@gmail.com>
2023-07-29 16:46:41 +0900

    add interface

--- src/token/IMyToken.sol
+++ src/token/IMyToken.sol
@@ -8,6 +8,10 @@ import {IVotes} from "../../lib/openzeppelin-contracts/contracts/governance/util
 interface IMyToken is IERC20, IERC165, IVotes {
     function initialSupply() external pure returns (uint256);
 
+    function updateGovernor(address newGovernor) external;
+
+    function mint(uint256 amount) external;
+
     function burn(uint256 amount) external;
 
     function burnFrom(address account, uint256 amount) external;
